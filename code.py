360f5ae776df8d7ceb81e7b58c6893d59ccfa061
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2020-10-22 10:23:10 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import pyautogui
 import unittest
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
